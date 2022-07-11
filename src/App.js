43fc1af9a759fd9c56784e1141157b4ca985919f
Armağan Dalkıran <armagandalkiran@Armagan-MacBook-Pro.local>
2022-07-11 18:00:45 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { React, useState } from "react";
+
+export const replaceCamelWithSpaces = (colorName) => colorName.replace(/\B([A-Z])\B/g," $1") 
+
+const buttonInitialState = {
+  color: "mediumvioletred",
+  text: `change to ${replaceCamelWithSpaces("MidnightBlue")}`,
+};
+
+const checkboxInitialState = false;
 
 function App() {
+  const [buttonStyle, setButtonStyle] = useState(buttonInitialState);
+  const [checkboxStyle, setCheckboxStyle] = useState(checkboxInitialState);
+
+  const handleButtonChange = () => {
+    buttonStyle.color === "mediumvioletred"
+      ? setButtonStyle({ color: "MidnightBlue", text: "change to Medium Violet Red" })
+      : setButtonStyle(buttonInitialState);
+  };
+
+  const handleCheckboxChange = () => {
+    setCheckboxStyle(!checkboxStyle);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button
+        onClick={handleButtonChange}
+        style={
+          checkboxStyle
+            ? { backgroundColor: "gray" }
+            : { backgroundColor: `${buttonStyle.color}` }
+        }
+        disabled={checkboxStyle}
+      >
+        {buttonStyle.text}
+      </button>
+      <input type="checkbox" onChange={handleCheckboxChange} />
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,74 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, fireEvent } from "@testing-library/react";
+import { replaceCamelWithSpaces } from "./App";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("button has correct initial color", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const colorbutton = screen.getByRole("button", {
+    name: "change to Midnight Blue",
+  });
+  expect(colorbutton).toHaveStyle({ backgroundColor: "mediumvioletred" });
+  fireEvent.click(colorbutton);
+  expect(colorbutton).toHaveStyle({ backgroundColor: "MidnightBlue" });
+  expect(colorbutton).toHaveTextContent("change to Medium Violet Red");
+});
+
+test("initial conditions", () => {
+  render(<App />);
+  const colorbutton = screen.getByRole("button", {
+    name: "change to Midnight Blue",
+  });
+  expect(colorbutton).toBeEnabled();
+  const checkbox = screen.getByRole("checkbox");
+  expect(checkbox).not.toBeChecked();
+});
+
+test("button and checkbox actions", () => {
+  render(<App />);
+  const colorButton = screen.getByRole("button", {
+    name: "change to Midnight Blue",
+  });
+  const checkbox = screen.getByRole("checkbox");
+  fireEvent.click(checkbox);
+  expect(colorButton).toBeDisabled();
+  expect(checkbox).toBeChecked();
+  fireEvent.click(checkbox);
+  expect(colorButton).toBeEnabled();
+});
+
+test("button is red and disabled", () => {
+  render(<App />);
+  const colorButton = screen.getByRole("button", {
+    name: "change to Midnight Blue",
+  });
+  const checkbox = screen.getByRole("checkbox");
+  fireEvent.click(checkbox);
+  expect(colorButton).toHaveStyle("background-color:gray");
+  fireEvent.click(checkbox);
+  expect(colorButton).toHaveStyle("background-color:mediumvioletred");
+});
+
+test("button is blue and disabled", () => {
+  render(<App />);
+  const colorButton = screen.getByRole("button", {
+    name: "change to Midnight Blue",
+  });
+  fireEvent.click(colorButton);
+  const checkbox = screen.getByRole("checkbox");
+  fireEvent.click(checkbox);
+  expect(colorButton).toHaveStyle("background-color:gray");
+  fireEvent.click(checkbox);
+  expect(colorButton).toHaveStyle("background-color:MidnightBlue");
+});
+
+describe("replace camels with spaces", () => {
+  test("one letter", () => {
+    expect(replaceCamelWithSpaces("red")).toBe("red");
+  });
+  test("two letter", () => {
+    expect(replaceCamelWithSpaces("redBlue")).toBe("red Blue");
+  });
+  test("three letter", () => {
+    expect(replaceCamelWithSpaces("redBlueWhite")).toBe("red Blue White");
+  });
 });
